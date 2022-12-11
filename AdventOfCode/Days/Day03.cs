959eaf8653d0fec393c221b97ab568befcbfe0a3
Rambolarsen <rambolarsen@gmail.com>
2022-12-11 08:50:12 +0100

    Add day 3

---
+++ AdventOfCode/Days/Day03.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode.Days
+{
+    public class Day03 : BaseDay
+    {
+        public override ValueTask<string> Solve_1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
